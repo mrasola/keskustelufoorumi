0e668e38d4b51e947dbe3b1bae2bfd1d52bd84b7
mrasola <miika.rasola@helsinki.fi>
2018-05-25 16:24:40 +0300

    week 2 2.3

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,25 @@
+# Tuodaan Flask käyttöön
 from flask import Flask
 app = Flask(__name__)
 
+# Tuodaan SQLAlchemy käyttöön
+from flask_sqlalchemy import SQLAlchemy
+
+# Käytetään tasks.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
+# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
+# samassa paikassa
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///messages.db"
+
+# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
+app.config["SQLALCHEMY_ECHO"] = True
+
+# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
+db = SQLAlchemy(app)
+
+
 from application import views
+from application.messages import models
+from application.messages import views
+
+# Luodaan lopulta tarvittavat tietokantataulut
+db.create_all()
